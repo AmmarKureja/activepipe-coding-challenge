bc0322b40dd45bb2d9ebef2d3d5001c28a6302d8
Ammar Hassan <ammar.hassan@hagridsolutions.com>
2019-05-01 12:43:58 +1000

    initial commit

---
+++ src/Layout/Layout.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+const layout = (props) => (
+    <React.Fragment>
+        <div>
+
+        </div>
+        <main>
+            {props.children}
+        </main>
+    </React.Fragment>
+)
+
+export default layout;
